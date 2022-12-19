cea55089a72377aa5fb94aaf16bd69d82fa1546d
younhwan97 <younhwan0903@gmail.com>
2022-12-20 04:14:16 +0900

    [Setting] IMDB API_KEY

--- app/src/main/java/kr/co/younhwan/boxoffice/common/Constants.kt
+++ app/src/main/java/kr/co/younhwan/boxoffice/common/Constants.kt
@@ -1,5 +1,9 @@
 package kr.co.younhwan.boxoffice.common
 
+import kr.co.younhwan.boxoffice.BuildConfig
+
 object Constants {
-    const val BASE_URL = ""
+    const val BASE_URL = "https://imdb-api.com/"
+
+    const val API_KEY = BuildConfig.API_KEY
 }
---
+++ app/src/main/java/kr/co/younhwan/boxoffice/data/remote/BoxOfficeApi.kt
@@ -0,0 +1,8 @@
+package kr.co.younhwan.boxoffice.data.remote
+
+interface BoxOfficeApi {
+
+
+}
+
+// http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.xml?key=97f73904a4cb56ba0ef21b162d65781d&targetDt=20221220

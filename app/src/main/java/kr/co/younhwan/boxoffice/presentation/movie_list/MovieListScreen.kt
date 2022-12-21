6bedcb2b8fa14f711c08e1d56d7fb87877a5d9d5
younhwan97 <younhwan0903@gmail.com>
2022-12-22 00:48:43 +0900

    [Add] LoadingScreen

--- app/src/main/java/kr/co/younhwan/boxoffice/presentation/movie_list/MovieListScreen.kt
+++ app/src/main/java/kr/co/younhwan/boxoffice/presentation/movie_list/MovieListScreen.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.airbnb.lottie.compose.*
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.calculateCurrentOffsetForPage
@@ -25,24 +30,24 @@ fun MovieListScreen(
 ) {
     val state = viewModel.state.value
 
-    if (!state.isLoading) {
-        val movies = state.movies
-        val pagerState = rememberPagerState()
+    Column() {
+        Text(
+            text = "Top",
+            style = MaterialTheme.typography.h1,
+            modifier = Modifier.padding(start = 16.dp, top = 40.dp),
+            fontWeight = FontWeight.Black
+        )
+        Text(
+            text = "Box Office",
+            style = MaterialTheme.typography.h2,
+            modifier = Modifier.padding(start = 16.dp),
+            color = MaterialTheme.colors.primary,
+            fontWeight = FontWeight.Black
+        )
 
-        Column {
-            Text(
-                text = "Top",
-                style = MaterialTheme.typography.h1,
-                modifier = Modifier.padding(start = 16.dp, top = 40.dp),
-                fontWeight = FontWeight.Black
-            )
-            Text(
-                text = "Box Office",
-                style = MaterialTheme.typography.h2,
-                modifier = Modifier.padding(start = 16.dp),
-                color = MaterialTheme.colors.primary,
-                fontWeight = FontWeight.Black
-            )
+        if (!state.isLoading) {
+            val movies = state.movies
+            val pagerState = rememberPagerState()
 
             HorizontalPager(
                 state = pagerState,
@@ -58,6 +63,30 @@ fun MovieListScreen(
                     movie = movies[index],
                     pageOffset = calculateCurrentOffsetForPage(index).absoluteValue,
                     onItemClick = { navController.navigate(Screen.MovieDetailScreen.route + "/${movies[index].id}") }
+                )
+            }
+        } else {
+            val composition by rememberLottieComposition(
+                LottieCompositionSpec.RawRes(kr.co.younhwan.boxoffice.R.raw.movie_loading)
+            )
+            val lottieAnimatable = rememberLottieAnimatable()
+
+            LaunchedEffect(composition) {
+                lottieAnimatable.animate(
+                    composition = composition,
+                    clipSpec = LottieClipSpec.Frame(0, 1200),
+                    initialProgress = 0f
+                )
+            }
+
+            Box(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                LottieAnimation(
+                    composition = composition,
+                    progress = lottieAnimatable.progress,
+                    contentScale = ContentScale.FillHeight,
+                    modifier = Modifier.align(Alignment.Center)
                 )
             }
         }

289b5b5d3cf92a2c9fd2eb1b145a0cbb2d4bc480
younhwan97 <younhwan0903@gmail.com>
2022-12-21 17:18:14 +0900

    [Remove] Unused import

--- app/src/main/java/kr/co/younhwan/boxoffice/presentation/movie_list/components/MovieListScreen.kt
+++ app/src/main/java/kr/co/younhwan/boxoffice/presentation/movie_list/components/MovieListScreen.kt
@@ -1,33 +1,17 @@
 package kr.co.younhwan.boxoffice.presentation.movie_list.components
 
-import android.widget.ImageView
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.util.lerp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
-import coil.load
-import com.flaviofaria.kenburnsview.KenBurnsView
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.calculateCurrentOffsetForPage
 import com.google.accompanist.pager.rememberPagerState
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.yield
 import kr.co.younhwan.boxoffice.presentation.movie_list.MovieListViewModel
 import kotlin.math.absoluteValue
 
@@ -39,7 +23,7 @@ fun MovieListScreen(
     val state = viewModel.state.value
 
     val pagerState = rememberPagerState()
-    
+
     if (!state.isLoading) {
         val movies = state.movies
 

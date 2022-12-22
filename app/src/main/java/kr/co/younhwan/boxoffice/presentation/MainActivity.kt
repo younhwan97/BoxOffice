a1ef4cc95099f7e6c2d770fa5a6e191a5b1dbc00
younhwan97 <younhwan0903@gmail.com>
2022-12-23 03:58:05 +0900

    [Remove] unused import

--- app/src/main/java/kr/co/younhwan/boxoffice/presentation/MainActivity.kt
+++ app/src/main/java/kr/co/younhwan/boxoffice/presentation/MainActivity.kt
@@ -3,12 +3,8 @@ package kr.co.younhwan.boxoffice.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
